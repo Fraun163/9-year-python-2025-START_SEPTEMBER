7244877733dfd6052a9684064721eb7ceadb36a5
Fraun163 <ilya.bystrov.2011@gmail.com>
2025-11-30 17:30:52 +0400

    3 изменение

--- 1/Homework.py
+++ 1/Homework.py
@@ -1,2 +1,19 @@
-a = int(input())
+#задание N3.1
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+a = 0
+for i in range(3):
+    for j in range(3,1,-1):
+        a += matrix[i][j]
 print(a)
+
+#задание N4
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+c = int(input())
+d = int(input())
+a = 0
+b = 0
+for i in range(3):
+    a += matrix[c][i]
+for i in range(3):
+    b += matrix[i][d]
+print(a,b)
